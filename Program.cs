706c8de2f028e1302e4bde22c87430891ffb5547
WIN-HIJ4FH2GJF8\user <jolahmilovski@gmail.com>
2024-09-17 23:10:40 +0500

    need add try catch

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace GuessTheWorld
     {
         static void Main(string[] args)
         {
+            string wordToGuess = null;
+
             Console.WriteLine("Welcome to 'Guess world!\n Do you want to type own words or guess?\n" +
                 "Type Y if you want to type own world or 'N' if you want a randomly selected word");
             string randomOrNot = Console.ReadLine();
@@ -13,8 +15,10 @@ namespace GuessTheWorld
 
             bool isValidResponse;
 
-            if ((randomOrNot == "Y") || (randomOrNot == "N")){
-                isValidResponse = true; }
+            if ((randomOrNot == "Y") || (randomOrNot == "N"))
+            {
+                isValidResponse = true; 
+            }
             else { 
                 isValidResponse = false;
             }
@@ -23,8 +27,10 @@ namespace GuessTheWorld
             while (isValidResponse == false) 
             {
                 Console.WriteLine("Only N or Y");
+
                 randomOrNot = Console.ReadLine();
                 randomOrNot = randomOrNot.ToUpper();
+
                 if ((randomOrNot == "Y") || (randomOrNot == "N"))
                 {
                     isValidResponse = true;
@@ -36,7 +42,7 @@ namespace GuessTheWorld
 
             }
             Console.Clear();
-            string wordToGuess = null;
+
 
             if (randomOrNot == "Y")
             {
@@ -53,12 +59,97 @@ namespace GuessTheWorld
                 wordToGuess = wordToGuess.ToUpper();
             }
 
-            else if (randomOrNot == "N")
+            else if (randomOrNot == "N")   // если выбрали угадывать слова из списка
             {
-                //C:\Users\user\source\repos\GuessTheWorld
-                var path = Path.Combine(@"...\...\...\WordsToGuess.txt");
-
+           
+                //"C:\\Users\\user\\source\\repos\\GuessTheWorld\\WordToGuess.txt"
+                string path = Path.Combine(@"C:\Users\user\source\repos\GuessTheWorld\WordToGuess.txt");
+                string[] line = File.ReadAllLines(path);
+                
+                Random random = new Random();   
+                int randomIndex = random.Next(line.Length);
+                wordToGuess = line[randomIndex];
+                wordToGuess = wordToGuess.ToUpper();
+                Console.Clear();
+                Console.WriteLine("Game Start");
             }
+            int wordLenth = wordToGuess.Length;
+            char[] positionsToGuess = new char[wordLenth];
+            char[] wordToGuessChar = wordToGuess.ToCharArray();
+            int lives = 5;
+
+            List<char> letterGuessed = new List<char>();
+
+            bool gameWon = false;
+
+            for (int i = 0; i < wordLenth; i++)
+            {
+                positionsToGuess[i] = '_';
+            }
+
+            while (lives > 0)
+            {
+                string printProgress = String.Concat(positionsToGuess);
+
+                bool letterFound = false;
+                int multiples = 0;
+                if (printProgress == wordToGuess)
+                {
+                    gameWon = true;
+                    break;
+                }
+
+                Console.WriteLine("You have {0} lives!", lives);
+                Console.WriteLine("Word to guess " + printProgress);
+                Console.WriteLine("\n Guess a letter:");
+
+                string playerGuessLetter = Console.ReadLine();
+
+                bool guessTest = playerGuessLetter.All(Char.IsLetter);
+
+                while (guessTest == false || playerGuessLetter.Length != 1)
+                {
+                    Console.WriteLine("Please input only a single letter");
+                    Console.WriteLine("\n Guess a letter:");
+                    playerGuessLetter = Console.ReadLine();
+                    guessTest = playerGuessLetter.All(Char.IsLetter);
+                }
+                playerGuessLetter = playerGuessLetter.ToUpper();
+
+                char playerChar = Convert.ToChar(playerGuessLetter);
+
+                if (letterGuessed.Contains(playerChar) == false)
+                {
+                    letterGuessed.Add(playerChar);
+
+                    for (int i = 0; i < wordToGuessChar.Length; i++)
+                    {
+                        if (wordToGuessChar[i] == playerChar)
+                        {
+                            positionsToGuess[i] = playerChar;
+                            letterFound = true;
+                            multiples++;
+                        }
+                    }
+
+                    if (letterFound)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Found {0} letter {1}!", multiples, playerChar);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("No letter{0}!", playerChar);
+                        lives--;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You already guessed {0}!", playerChar);
+                }
+            }                        
         }
     }
 }
